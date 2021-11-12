8d91bd01731b571db62298e4adc7948dd04d896f
ludo <85556848+ludolfaugust@users.noreply.github.com>
2021-11-12 15:55:08 +0100

    add logo

---
+++ src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from "react";
+import CardItems from "./CardItems";
+import "./Cards.css";
+
+function Cards() {
+  return (
+    <div className="cards">
+      <h1> Get to know these epic indian Experiences </h1>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__items">
+            <CardItems
+              src="./images/img-5.jpg"
+              text="Discover India by Motorcycle - the great britsh Royal Enfield"
+              label="The Indian adventure"
+              path="/services"
+            />
+            <CardItems
+              src="./images/img-6.jpg"
+              text="Discover different cultures and religions on you trip"
+              label="The Indian adventure"
+              path="/services"
+            />
+          </ul>
+          <ul className="cards__items">
+            <CardItems
+              src="./images/img-3.jpg"
+              text="Enjoy the beauty of the nature in the so called Sundar Bans"
+              label="The Indian adventure"
+              path="/services"
+            />
+            <CardItems
+              src="./images/img-4.jpg"
+              text="Pick one of many goddess to make your favorite like Ganesha"
+              label="The Indian adventure"
+              path="/services"
+            />
+            <CardItems
+              src="./images/img-8.jpg"
+              text="Explore the himalayan tracks and villages by motorcycle"
+              label="The Indian adventure"
+              path="/services"
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
---
+++ src/components/HeroSection.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import "../App.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <h1>PACK YOUR BAGS</h1>
+      <div className="road-icon">
+        <p>The road is waiting for you...</p>
+        <i class="fas fa-road"></i>
+      </div>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          Let´s go
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+        >
+          Meet us
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
