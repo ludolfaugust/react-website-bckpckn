583800acbead78b3645678e5b6c7307dc59df745
ludo <85556848+ludolfaugust@users.noreply.github.com>
2021-11-17 16:27:20 +0100

    delete bugs

--- src/components/CardItems.js
+++ src/components/CardItems.js
@@ -9,7 +9,7 @@ function CardItems(props) {
           <figure className="cards__item__pic-wrap" data-category={props.label}>
             <img
               src={props.src}
-              alt="Travel Image"
+              alt="Travel through India"
               className="cards__item__img"
             />
           </figure>
