a54c43eb13a8e409c617a41a23798f2df454add2
ludo <85556848+ludolfaugust@users.noreply.github.com>
2021-11-18 12:47:32 +0100

    add Cards

--- src/components/pages/Joinus.js
+++ src/components/pages/Joinus.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "../../App.css";
 import Footer from "../Footer";
+import Cards from "../Cards";
 
 function Joinus() {
   return (
     <div>
       <h1 className="sign-up">JOIN US</h1>
+      <Cards />
       <Footer />
     </div>
   );
