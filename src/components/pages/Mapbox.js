8ee81442bafdb910b21de4c6e0ce8e1a8b9b2b51
ludolfkatte <85556848+ludolfaugust@users.noreply.github.com>
2022-03-09 14:20:53 +0100

    add h2

--- src/components/pages/Mapbox.js
+++ src/components/pages/Mapbox.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Mapbox() {
-  return <div>Mapbox</div>;
+  return (
+    <>
+      <div>Mapbox</div>
+
+      <h2>This is where it all starts - Plan you Trip</h2>
+    </>
+  );
 }
 
 export default Mapbox;
